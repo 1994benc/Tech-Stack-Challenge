c0411d0889f91d6e119bd0869c9417cfcca6a163
Ben Chomsang <1994benc@gmail.com>
2020-09-24 12:05:51 +0100

    Modify 'fetchCurrentTime' to resolve a string and reject an error message

--- features/step_definitions/displayCurrentTime.js
+++ features/step_definitions/displayCurrentTime.js
@@ -1,19 +1,20 @@
 const assert = require('assert');
-const { Given, When, Then } = require('@cucumber/cucumber');
+const { Given, When, Then, After } = require('@cucumber/cucumber');
 const axios = require('axios');
 const sinon = require('sinon')
 const moment = require('moment')
 
+/** ------------------------------------
+ * ---------BUSINESS LOGIC PLACEHOLDER---
+ * ------------------------------------*/ 
 // TODO: Move into business logic
 // TODO: Use RxJS
 function fetchCurrentTime() {
   return new Promise((resolve, reject) => axios.get('http://worldtimeapi.org/api/ip').then(function (response) {
-    console.log(response.data);
-    resolve(response)
+    resolve(createTimeString(new Date(response.data.datetime)))
   })
     .catch(function (error) {
-      console.log(error);
-      reject(error)
+      reject(createErrorMessage())
     }));
 }
 // TODO: Move into business logic
@@ -21,7 +22,14 @@ function createTimeString(dateObject) {
   return moment(dateObject).format("HH:mm:ss")
 }
 
+// TODO: Move into business logic
+function createErrorMessage() {
+    return "Error - back soon!"
+}
 
+/** ----------------------------
+ * ---------STEP--DEFINITIONS---
+ * -----------------------------*/ 
 
 Given('There is no error loading the current time', function () {
   const fakeResponse = {
@@ -32,20 +40,33 @@ Given('There is no error loading the current time', function () {
     }
   }
   // 10 seconds later is '2020-09-24T10:23:20.331886+01:00'
-  sinon.stub(axios, "get").resolves(fakeResponse)
-
+  this.getStubSandbox = sinon.createSandbox();
+  this.getStubSandbox.stub(axios, "get").resolves(fakeResponse)
 });
 
 When('A user loads the website', async function () {
-  // Write code here that turns the phrase above into concrete actions
-  const timeData = await fetchCurrentTime()
-  this.fetchedTimeData = timeData
-  return this.fetchedTimeData
+  try {
+    const timeData = await fetchCurrentTime()
+    this.fetchedTimeData = timeData
+  } catch (error) { 
+    this.errorMessage = error
+  }
 });
 
 Then('The current time is displayed', function () {
-  // Write code here that turns the phrase above into concrete actions
   const actualCurrentTime = createTimeString(new Date('2020-09-24T10:23:10.331886+01:00'))
-  const fetchedCurrentTime = createTimeString(new Date(this.fetchedTimeData.data.datetime))
+  const fetchedCurrentTime = this.fetchedTimeData
+  this.getStubSandbox.restore()
   return assert.strictEqual(actualCurrentTime, fetchedCurrentTime);
+});
+
+Given('There is an error loading the current time', function () {
+  // Stub an error 
+  this.getStubSandbox = sinon.createSandbox()
+  this.getStubSandbox.stub(axios, "get").rejects({ errorCode: 000, message: "Something went wrong" })
+});
+
+Then('The error message is displayed', function () {
+  const actualErrorMessage = "Error - back soon!"
+  return assert.strictEqual(actualErrorMessage, this.errorMessage);
 });
