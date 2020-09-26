e243013974cf62ee1d9a12cec982279aaa4d92c0
Ben Chomsang <1994benc@gmail.com>
2020-09-26 13:28:38 +0100

    Add business logic methods to DisplayTime
    To be implemented later

--- src/display-time/display-time.ts
+++ src/display-time/display-time.ts
@@ -2,5 +2,41 @@ import axios from 'axios'
 import * as moment from 'moment'
 
 export class DisplayTime {
-  
+  private _displayMessage: string
+
+  public get displayMessage(): string {
+    return this._displayMessage
+  }
+
+  async attached() {
+    await this.fetchCurrentTime()
+  }
+
+  public fetchCurrentTime(): Promise<string> {
+    const APIPath = 'http://worldtimeapi.org/api/ip'
+    return new Promise((resolve, reject) => {
+      axios
+        .get(APIPath)
+        .then((response) => {
+          const timeString = this.createTimeString(
+            new Date(response.data.datetime),
+          )
+          resolve(timeString)
+        })
+        .catch((error) => {
+          const errorMessage = this.createErrorMessage()
+          reject(errorMessage)
+        })
+    })
+  }
+
+  public createTimeString(dateObject: Date): string {
+    return moment(dateObject).format('HH:mm:ss')
+  }
+
+  public createErrorMessage(): string {
+    return 'Error - back soon!'
+  }
+
+  public automaticReload(): void {}
 }
