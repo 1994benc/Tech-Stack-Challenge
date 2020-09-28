bbe6756723954564dcc8bcd8cee827b1bb53ac51
Ben Chomsang <1994benc@gmail.com>
2020-09-28 01:57:16 +0100

    compile components into js before starting compileCucumber gulp task

--- build/tasks/compile-cucumber.js
+++ build/tasks/compile-cucumber.js
@@ -2,13 +2,22 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var paths = require('../paths');
 
-gulp.task('compileCucumber', function () {
-  return gulp.src(paths.cucumberStepDefs+"*.ts")
+gulp.task('compileComponents', function () {
+  return gulp.src(paths.source).pipe(ts({
+    noImplicitAny: true,
+    // outFile: 'output.js',
+    target: "ES5",
+    module: "umd"
+  })).pipe(gulp.dest(paths.root))
+})
+
+gulp.task('compileCucumber', ['compileComponents'], function () {
+  return gulp.src(paths.cucumberStepDefs + "*.ts")
     .pipe(ts({
       noImplicitAny: true,
       // outFile: 'output.js',
-      target:"ES5",
-      module:"umd"
+      target: "ES5",
+      module: "umd"
     }))
     .pipe(gulp.dest(paths.cucumberStepDefs));
 });
